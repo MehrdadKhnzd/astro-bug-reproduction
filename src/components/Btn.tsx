e25a19a089cd2d9b963ccccf133ecdb04521776a
MehrdadKhnzd <mehrdadkhnzd@gmail.com>
2024-04-07 23:29:22 -0400

    feat: change onClick structure

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -3,9 +3,9 @@ import { createSignal, onMount } from 'solid-js';
 export default () => {
   const [num, setNum] = createSignal(0);
 
-  const inc = () => setNum((v) => v + 1);
+  // const inc = () => setNum((v) => v + 1);
 
-  onMount(() => setNum(20));
+  // onMount(() => setNum(20));
 
-  return <button onClick={inc}>{num()}</button>;
+  return <button onClick={[setNum, 10]}>{num()}</button>;
 };
